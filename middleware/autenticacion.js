ad3cb215db04b754eab993d812802338d8d2f9a8
Jorge Vanegas <jvanegasv@gmail.com>
2018-01-22 16:47:52 -0600

    Aceptar el token como parametro del URL o como header

--- middleware/autenticacion.js
+++ middleware/autenticacion.js
@@ -6,12 +6,16 @@ var SEED = require('../config/config').SEED;
 exports.verificaToken = function(req, res, next) {
 
     var token = req.query.token;
+    if (!token) {
+        token = req.get('authorization').replace('Bearer ', '');
+    }
     jwt.verify(token, SEED, (err, decoded) => {
 
         if (err) {
             return res.status(401).json({
                 ok: false,
                 mensaje: 'Token invalido',
+                headers: req.get('authorization'),
                 errors: err
             });
         }
