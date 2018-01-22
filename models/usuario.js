4a6e913b5c1855309119455681d9b4fc9439198d
Jorge Vanegas <jvanegasv@gmail.com>
2018-01-22 13:43:14 -0600

    Clase 103 ok

--- models/usuario.js
+++ models/usuario.js
@@ -1,24 +1,27 @@
 var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
 
 var Schema = mongoose.Schema;
+
+var rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un role valido'
+};
 
 var usuarioSchema = new Schema({
 
     nombre: {
         type: String,
         required: [true, 'El nombre es requerido'],
-
     },
     email: {
         type: String,
         unique: true,
         required: [true, 'El correo es requerido'],
-
     },
     password: {
         type: String,
         required: [true, 'El password es requerido'],
-
     },
     img: {
         type: String
@@ -27,9 +30,13 @@ var usuarioSchema = new Schema({
         type: String,
         required: [true, 'El nombre es requerido'],
         default: 'USER_ROLE',
-
+        enum: rolesValidos
     }
 
+});
+
+usuarioSchema.plugin(uniqueValidator, {
+    message: '{PATH} debe ser unico'
 });
 
 module.exports = mongoose.model('Usuario', usuarioSchema);
