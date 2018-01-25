3971091632dd543d341345cfc0bf5a79ee260ce2
Jorge Vanegas <jvanegasv@gmail.com>
2018-01-25 11:23:35 -0600

    Clase 139 ok - verificacion de token de google en backend

--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
 module.exports.SEED = 'h3g0y465';
+
+// google
+module.exports.GOOGLE_CLIENT_ID = '723672426611-3o9sgungqmubg0bftfnim5nelsg3mfbe.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = 'Dnd7-QQOvHGghPTY1rr8X5K-';
--- routes/login.js
+++ routes/login.js
@@ -6,6 +6,48 @@ var SEED = require('../config/config').SEED;
 var app = express();
 var Usuario = require('../models/usuario');
 
+const GoogleAuth = require('google-auth-library');
+// var auth = new GoogleAuth;
+
+const GOOGLE_CLIENT_ID = require('../config/config').GOOGLE_CLIENT_ID;
+const GOOGLE_SECRET = require('../config/config').GOOGLE_SECRET;
+
+// autenticacion de google
+app.post('/google', (req, res) => {
+
+    var token = req.body.token || '';
+
+    var client = new GoogleAuth.OAuth2Client(GOOGLE_CLIENT_ID, GOOGLE_SECRET, '');
+
+    client.verifyIdToken({
+            idToken: token,
+            audience: GOOGLE_CLIENT_ID
+        },
+        function(e, login) {
+
+            if (e) {
+                return res.status(400).json({
+                    ok: false,
+                    errors: e
+                });
+            }
+
+            var payload = login.getPayload();
+            var userid = payload['sub'];
+            // If request specified a G Suite domain:
+            //var domain = payload['hd'];
+
+            res.status(200).json({
+                ok: true,
+                payload: payload
+            });
+        }
+    );
+
+
+});
+
+// autenticacion normal
 app.post('/', (req, res) => {
 
     var body = req.body;
